6b7bac6df41c94d65e17acf7e923433184c1e713
Aby <abithomas520@gmail.com>
2024-10-18 13:18:06 +0530

    Update string.py

--- factorial.py
+++ factorial.py
@@ -1,17 +1,4 @@
-def fact(n):
-    if n==0 or n==1:
-     return 1
-    else:
-        return n*fact(n-1)
-while True:
-
-n=int(input("Enter the number or -1 to exit\n"))
-
-if n==-1:
-   print("Exiting")
-   break
-
-result=fact(n)
-
-print(f"The factorial of number {n} is {result}")
-    
+input_string = input("Enter a string: ")
+first_char = input_string[0]
+modified_string = first_char + input_string[1:].replace(first_char, '$')
+print("Modified string:", modified_string)
